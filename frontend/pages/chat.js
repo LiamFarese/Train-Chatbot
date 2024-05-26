68022a0e8e3d85b006ad55e47fb67e9b6781eaf2
Platymek <hectordavid.selby@gmail.com>
2024-05-26 13:11:16 +0100

    Updated Help page
    - Updated the help page to say:
    - - how to use the app
    - - credits for who made the app
    
    TODO:
    - fix dark mode integration (if possible)

--- frontend/pages/chat.js
+++ frontend/pages/chat.js
@@ -157,6 +157,7 @@ export default function Chat() {
                     <Button
 
                         onPress={() => setCurrentModal('Help')}
+                        primary={true}
                     >
                         Help
 
